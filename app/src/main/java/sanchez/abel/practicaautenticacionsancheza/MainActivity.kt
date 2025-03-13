fd0e0cac543bd6a93f237047915494fd178fa95b
abel2606 <abel.sanchez.guerrero@gmail.com>
2025-03-12 22:08:47 -0700

    Contenido de presentaciones

--- app/src/main/java/sanchez/abel/practicaautenticacionsancheza/MainActivity.kt
+++ app/src/main/java/sanchez/abel/practicaautenticacionsancheza/MainActivity.kt
@@ -1,10 +1,15 @@
 package sanchez.abel.practicaautenticacionsancheza
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +21,18 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val email = intent.extras!!.getString("user")
+
+        val user: TextView = findViewById(R.id.tvUser)
+        val button: Button = findViewById(R.id.btnLogout)
+
+        button.setOnClickListener({
+            Firebase.auth.signOut()
+            val intent = Intent(this, LoginActivity::class.java)
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
+
+        })
     }
 }
